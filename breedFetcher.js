1455b364386d4fe5eadebf385dc28b4d1f9a9f4b
William <williamwijaya.mail@gmail.com>
2022-04-14 21:45:30 +0000

    Cats as a service

--- breedFetcher.js
+++ breedFetcher.js
@@ -19,7 +19,7 @@ request(`https://api.thecatapi.com/v1/breeds/search?q=${breedSearch}`, (error, r
   console.log(typeof data);
 
   if (data.length === 0) {
-    console.log("Sorry, can not display the breed information")
+    console.log("Sorry, can not display the breed information");
   }
   
 });
@@ -28,5 +28,5 @@ request(`https://api.thecatapi.com/v1/breeds/search?q=${breedSearch}`, (error, r
 
 // console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
 // console.log('error:', error); // Print the error if one occurred
-// console.log('body:', body); // Print the HTML for the Google homepage.  
+// console.log('body:', body); // Print the HTML for the Google homepage.
 // console.log(typeof body);
